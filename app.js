c2f89ef1a168a999d1683575f7cd07f1e34148a9
Chris Song <chris.bk.song@gmail.com>
2020-05-18 11:31:50 -0400

    updates files

--- app.js
+++ app.js
@@ -5,10 +5,25 @@ const PORT = 3000;
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+const data = [
+  {
+    id: 1,
+    text: 'Hot Garbage',
+  },
+  {
+    id: 2,
+    text: "Filet o' Tennis Shoe",
+  },
+  {
+    id: 3,
+    text: 'Apple Core Ice Cream',
+  },
+];
+
 app.get('/about', (req, res) => {
   res.render('about', {
     title: 'Trash Restaurant',
-    special: 'Hot Garbage',
+    special: data,
   });
 });
 
